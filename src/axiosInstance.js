c29385d3f43cda4b6c753e168e0b067ae2e911b6
hjkar1 <hjkar1@outlook.com>
2019-08-30 07:13:01 +0300

    add separate backend API urls for dev and prod environments

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+// The development environment uses locally running API and production environment uses an external API.
+
+const apiUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_PROD_API
+    : process.env.REACT_APP_DEV_API;
+
 const instance = axios.create({
-  baseURL: `${process.env.REACT_APP_BACKEND}`
+  baseURL: `${apiUrl}`
 });
 
 export default instance;
