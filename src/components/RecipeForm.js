4b013840a9593a8c84ba81b1ae31da805229e5e3
hjkar1 <hjkar1@outlook.com>
2019-09-20 11:50:24 +0300

    refactor RecipeForm component

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -17,61 +17,54 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const RecipeForm = ({ handleSubmit, recipe }) => {
+const RecipeForm = ({
+  handleSubmit,
+  handleChange,
+  recipe: { title, instructions, ingredients }
+}) => {
   const classes = useStyles();
-
-  const [title, setTitle] = useState('');
-  const [ingredients, setIngredients] = useState('');
-  const [instructions, setInstructions] = useState('');
-
-  useEffect(() => {
-    if (recipe) {
-      setTitle(recipe.title);
-      setIngredients(recipe.ingredients);
-      setInstructions(recipe.instructions);
-    }
-  }, [recipe]);
 
   return (
     <div className={classes.container}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextField
           autoFocus
           margin="dense"
           id="title"
+          name="title"
           label="Recipe title"
           className={classes.textField}
           fullWidth
           value={title}
-          onChange={({ target }) => setTitle(target.value)}
+          onChange={handleChange}
         />
         <TextField
           margin="dense"
           id="ingredients"
+          name="ingredients"
           label="Ingredients"
           className={classes.textField}
           fullWidth
           multiline
           value={ingredients}
-          onChange={({ target }) => setIngredients(target.value)}
+          onChange={handleChange}
         />
         <TextField
           margin="dense"
           id="instructions"
+          name="instructions"
           label="Instructions"
           className={classes.textField}
           fullWidth
           multiline
           value={instructions}
-          onChange={({ target }) => setInstructions(target.value)}
+          onChange={handleChange}
         />
         <Button
-          onClick={event =>
-            handleSubmit(event, { title, ingredients, instructions })
-          }
           color="primary"
           disabled={title.length < 1 || ingredients.length < 1}
           className={classes.button}
+          type="submit"
         >
           Save
         </Button>
