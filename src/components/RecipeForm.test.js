d88d2b795382dbcf9b251316ea702759f00acbba
hjkar1 <hjkar1@outlook.com>
2019-09-25 16:48:25 +0300

    add unit test for RecipeForm component

--- src/components/RecipeForm.test.js
+++ src/components/RecipeForm.test.js
@@ -1,54 +1,103 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import RecipeForm from './RecipeForm';
 
-/* Unit test for RecipeForm component. */
+/* Unit tests for RecipeForm component. */
 
-test('enables form submit only if the form contains title and ingredients', () => {
-  const recipe1 = { title: '', ingredients: '', instructions: '' };
+describe('<RecipeForm />', () => {
+  // A helper component that renders the tested component
+  // and keeps the text entered to the tested component in its state.
+  const Wrapper = ({ recipe, onSubmit }) => {
+    const onChange = ({ target: { name, value } }) => {
+      recipe[name] = value;
+    };
 
-  const recipe2 = {
-    title: 'Test title',
-    ingredients: '',
-    instructions: ''
+    return (
+      <RecipeForm
+        recipe={recipe}
+        handleSubmit={onSubmit}
+        handleChange={onChange}
+      />
+    );
   };
 
-  const recipe3 = {
-    title: 'Test title',
-    ingredients: 'Test ingredients',
-    instructions: ''
-  };
+  test('updates the state of the parent component and submits form', () => {
+    const recipe = { title: '', ingredients: '', instructions: '' };
+    const mockSubmit = jest.fn();
+    const submittedRecipe = {
+      title: 'Test title',
+      ingredients: 'Test ingredients',
+      instructions: 'Test instructions'
+    };
 
-  const { getByText, rerender } = render(
-    <RecipeForm
-      recipe={recipe1}
-      handleSubmit={jest.fn}
-      handleChange={jest.fn}
-    />
-  );
+    const component = render(<Wrapper onSubmit={mockSubmit} recipe={recipe} />);
 
-  const submitButton = getByText('Save');
+    const titleInput = component.container.querySelector('#title');
+    const ingredientsInput = component.container.querySelector('#ingredients');
+    const instructionsInput = component.container.querySelector(
+      '#instructions'
+    );
+    const form = component.container.querySelector('form');
 
-  expect(submitButton).toBeDisabled();
+    fireEvent.change(titleInput, { target: { value: 'Test title' } });
+    fireEvent.change(ingredientsInput, {
+      target: { value: 'Test ingredients' }
+    });
+    fireEvent.change(instructionsInput, {
+      target: { value: 'Test instructions' }
+    });
+    fireEvent.submit(form);
 
-  rerender(
-    <RecipeForm
-      recipe={recipe2}
-      handleSubmit={jest.fn}
-      handleChange={jest.fn}
-    />
-  );
+    expect(mockSubmit.mock.calls.length).toBe(1);
+    expect(recipe).toEqual(submittedRecipe);
+  });
 
-  expect(submitButton).toBeDisabled();
+  test('enables form submit only if the form contains title and ingredients', () => {
+    const recipe1 = { title: '', ingredients: '', instructions: '' };
 
-  rerender(
-    <RecipeForm
-      recipe={recipe3}
-      handleSubmit={jest.fn}
-      handleChange={jest.fn}
-    />
-  );
+    const recipe2 = {
+      title: 'Test title',
+      ingredients: '',
+      instructions: ''
+    };
 
-  expect(submitButton).not.toBeDisabled();
+    const recipe3 = {
+      title: 'Test title',
+      ingredients: 'Test ingredients',
+      instructions: ''
+    };
+
+    const { getByText, rerender } = render(
+      <RecipeForm
+        recipe={recipe1}
+        handleSubmit={jest.fn}
+        handleChange={jest.fn}
+      />
+    );
+
+    const submitButton = getByText('Save');
+
+    expect(submitButton).toBeDisabled();
+
+    rerender(
+      <RecipeForm
+        recipe={recipe2}
+        handleSubmit={jest.fn}
+        handleChange={jest.fn}
+      />
+    );
+
+    expect(submitButton).toBeDisabled();
+
+    rerender(
+      <RecipeForm
+        recipe={recipe3}
+        handleSubmit={jest.fn}
+        handleChange={jest.fn}
+      />
+    );
+
+    expect(submitButton).not.toBeDisabled();
+  });
 });
