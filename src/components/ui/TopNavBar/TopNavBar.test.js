a130ce8beb7b38b485780166479635ebad038a29
hjkar1 <hjkar1@outlook.com>
2019-10-03 11:37:51 +0300

    remove unused import

--- src/components/ui/TopNavBar/TopNavBar.test.js
+++ src/components/ui/TopNavBar/TopNavBar.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, fireEvent } from 'unit-test-utils';
+import { render } from 'unit-test-utils';
 import { TopNavBar } from './TopNavBar';
 
 // Use routing to test redirect after logout.
