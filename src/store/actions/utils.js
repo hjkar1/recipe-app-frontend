ce6816d48f1c990dc1fb69dcd31c98020a467d2b
hjkar1 <hjkar1@outlook.com>
2019-10-17 15:01:58 +0300

    refactor a helper function, modify comments

--- src/store/actions/utils.js
+++ src/store/actions/utils.js
@@ -1,13 +1,15 @@
-// Get auth token from local storage and add to header
+// Get token from local storage and add to authorization header.
 
 export const getAuthHeaderConfig = () => {
   const user = window.localStorage.getItem('loggedInUser');
+
   if (user) {
-    const userObject = JSON.parse(user);
-    const token = userObject.token;
+    const { token } = JSON.parse(user);
+
     const config = {
       headers: { Authorization: `bearer ${token}` }
     };
+
     return config;
   }
 
